c2e6c1057311508e4eb759399910dc6accf5104e
l4p1n <l4p1n@l4p1n.ch>
2026-05-04 23:19:55 +0200

    Create packet types for Mindustry encoding/decoding

---
+++ NetClajServer/Packets/IMindustryPacket.cs
@@ -0,0 +1,11 @@
+﻿using System.Net.Sockets;
+
+namespace NetClajServer.Packets;
+
+public interface IMindustryPacket
+{
+    public sbyte GetPacketType();
+    public byte GetPacketIdentifier();
+    public void Deserialize(BinaryReader reader);
+    public void Serialize(BinaryWriter writer);
+}
---
+++ NetClajServer/Packets/RegisterTcpPacket.cs
@@ -0,0 +1,24 @@
+﻿using NetClajServer.Datastructures;
+
+namespace NetClajServer.Packets;
+
+public class RegisterTcpPacket: IMindustryPacket
+{
+    public int ConnectionId { get; set; }
+
+    public const sbyte Type = PacketType.Framework;
+    public const byte Identifier = 4;
+
+    sbyte IMindustryPacket.GetPacketType() => Type;
+    public byte GetPacketIdentifier() => Identifier;
+
+    public void Deserialize(BinaryReader reader)
+    {
+        ConnectionId = reader.ReadInt32BigEndian();
+    }
+
+    public void Serialize(BinaryWriter writer)
+    {
+        writer.WriteInt32BigEndian(ConnectionId);
+    }
+}
