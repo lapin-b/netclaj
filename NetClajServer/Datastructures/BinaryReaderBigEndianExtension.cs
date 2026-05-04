af928cd611c55bc73976df6cbe9be2baf1391960
l4p1n <l4p1n@l4p1n.ch>
2026-05-04 23:11:59 +0200

    I guess we're doing extension methods to read BE numbers now

---
+++ NetClajServer/Datastructures/BinaryReaderBigEndianExtension.cs
@@ -0,0 +1,23 @@
+﻿using System.Buffers.Binary;
+
+namespace NetClajServer.Datastructures;
+
+// https://github.com/dotnet/runtime/issues/26904#issuecomment-2292530322
+public static class BinaryReaderBigEndianExtension
+{
+    public static short ReadInt16BigEndian(this BinaryReader br)
+    {
+        return BinaryPrimitives.ReadInt16BigEndian(br.ReadSpan(stackalloc byte[sizeof(short)]));
+    }
+
+    public static int ReadInt32BigEndian(this BinaryReader br)
+    {
+        return BinaryPrimitives.ReadInt32BigEndian(br.ReadSpan(stackalloc byte[sizeof(int)]));
+    }
+
+    private static ReadOnlySpan<byte> ReadSpan(this BinaryReader br, Span<byte> buffer)
+    {
+        br.BaseStream.ReadExactly(buffer);
+        return buffer;
+    }
+}
