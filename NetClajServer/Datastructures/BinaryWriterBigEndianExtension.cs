12cb2abb719a5f3917ae20cd5657c5b5fdea80e5
l4p1n <l4p1n@l4p1n.ch>
2026-05-04 19:22:18 +0200

    I guess we're doing extension methods to write BE numbers now

---
+++ NetClajServer/Datastructures/BinaryWriterBigEndianExtension.cs
@@ -0,0 +1,20 @@
+﻿using System.Buffers.Binary;
+
+namespace NetClajServer.Datastructures;
+
+public static class BinaryWriterBigEndianExtension
+{
+    public static void WriteInt16BigEndian(this BinaryWriter bw, short val)
+    {
+        Span<byte> buffer = stackalloc byte[sizeof(short)];
+        BinaryPrimitives.WriteInt16BigEndian(buffer, val);
+        bw.Write(buffer);
+    }
+
+    public static void WriteInt32BigEndian(this BinaryWriter bw, int val)
+    {
+        Span<byte> buffer = stackalloc byte[sizeof(int)];
+        BinaryPrimitives.WriteInt32BigEndian(buffer, val);
+        bw.Write(buffer);
+    }
+}
