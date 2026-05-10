ed6da17c25d82a6884d21ffd922762077aa30a8a
l4p1n <l4p1n@l4p1n.ch>
2026-05-10 22:02:12 +0200

    Here goes a benchmark I guess

---
+++ Benchmark/Program.cs
@@ -0,0 +1,38 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using NetClajServer.Packets;
+using NetClajServer.Packets.Claj;
+
+namespace Benchmark;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        BenchmarkRunner.Run<Benchmark>();
+    }
+}
+
+[MemoryDiagnoser]
+public class Benchmark
+{
+    private static readonly byte[] Payload = [
+            0xfc, 0x03, 0x4a, 0x01, 0x42, 0xa1 
+        ];
+    
+    [Benchmark]
+    public MindustryPacket BenchmarkDeserializer()
+    {
+        return NetClajServer.Mindustry.Serializer.Deserialize(Payload);
+    }
+    
+    [Benchmark]
+    public byte[] BenchmarkSerializer()
+    {
+        return NetClajServer.Mindustry.Serializer.Serialize(new ConnectionJoinPacket
+        {
+            RoomId = long.MaxValue,
+            ConnectionId = int.MaxValue
+        });
+    }
+}
---
+++ NetClajServer/Claj/RoomFactory.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+using NetClajServer.Mindustry;
+
+namespace NetClajServer.Claj;
+
+public class RoomFactory
+{
+    private readonly ILoggerFactory _loggerFactory;
+
+    public RoomFactory(ILoggerFactory loggerFactory)
+    {
+        _loggerFactory = loggerFactory;
+    }
+
+    public Room Create(Connection host, Func<long, bool> roomIdExists)
+    {
+        long roomId;
+
+        do
+        {
+            roomId = Random.Shared.NextInt64();
+        } while (!roomIdExists(roomId));
+        
+        //return new Room(roomId, host, _loggerFactory.CreateLogger<Room>());
+        throw new NotImplementedException();
+    }
+}
