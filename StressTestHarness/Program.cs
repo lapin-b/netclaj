26925015e860aa2ec2821168ce0c252d78e5c4ce
l4p1n <l4p1n@l4p1n.ch>
2026-05-25 15:56:36 +0200

    Add a rotation of rooms to the server

--- StressTestHarness/Program.cs
+++ StressTestHarness/Program.cs
@@ -1,64 +1,120 @@
 ﻿using System.Diagnostics;
+using System.IO.Pipes;
 
 namespace StressTestHarness;
 
 class Program
 {
-    private const int ClientsCount = 10;
-    private const int RoomsCount = 1;
+    private const int ClientsCount = 200;
+    private const int RoomsCount = 10;
+    private const double RotationRate = .1;
 
+    private const string HostToTest = "127.0.0.1";
+    private const int HostPortTotest = 7000;
+
+    private static readonly CancellationTokenSource GlobalCancel = new();
+    private static readonly List<MindustryClient> Clients = [];
+    private static readonly List<long> RoomIds = [];
+    
     static async Task Main(string[] args)
     {
         MindustryClient.GeneratedPacketsPerSecond = 5;
         MindustryClient.GeneratedPacketsJitter = .3;
         
         Debug.Assert(ClientsCount >= RoomsCount);
+        Console.CancelKeyPress += new ConsoleCancelEventHandler(HandleCancellation());
         
-        var globalCancel = new CancellationTokenSource();
-        Console.CancelKeyPress += new ConsoleCancelEventHandler(HandleCancellation(globalCancel));
-        
-        var clients = new List<MindustryClient>();
-        var roomIds = new List<long>();
-
         Console.WriteLine("Connecting and handshaking");
         for (var i = 0; i < ClientsCount; i++)
         {
-            var client = new MindustryClient("127.0.0.1", 7000, globalCancel.Token);
-            await client.ExecuteHandshake();
-            clients.Add(client);
+            var client = await CreateConnection(HostToTest, HostPortTotest, i < RoomsCount);
+            Clients.Add(client);
+
+            if (client.IsHost)
+            {
+                RoomIds.Add((long)client.RoomId!);
+                client.Run();
+            }
         }
 
-        Console.WriteLine("Creating rooms");
-        await Task.WhenAll(clients[..RoomsCount].Select(h => h.CreateRoom()));
-        roomIds = clients.Where(c => c.IsHost).Select(c => (long)c.RoomId!).ToList();
-        
         Console.WriteLine("Joining rooms");
-        foreach (var client in clients.Where(c => c.IsClient))
+        foreach (var client in Clients.Where(c => c.IsClient))
         {
-            var roomId = roomIds[Random.Shared.Next(0, roomIds.Count)];
-            Console.WriteLine($"Joining {client.ConnectionId} into {roomId}");
+            var roomId = RoomIds[Random.Shared.Next(0, RoomIds.Count)];
             await client.JoinRoom(roomId);
+            client.Run();
+            
+            Console.WriteLine($"Joined {client.ConnectionId} into {roomId}");
         }
 
-        foreach (var client in clients)
-        {
-            client.Run();
-        }
+        _ = RotateClientRoomsLoop();
         
-        globalCancel.Token.WaitHandle.WaitOne();
+        GlobalCancel.Token.WaitHandle.WaitOne();
         Console.WriteLine("Closing connections (and it's fine if we can't clean everything I guess)");
-        foreach (var client in clients)
+        foreach (var client in Clients)
         {
             client.Stop();
         }
     }
 
-    static Action<object?, ConsoleCancelEventArgs> HandleCancellation(CancellationTokenSource globalCancel)
+    static async Task RotateClientRoomsLoop()
+    {
+        GlobalCancel.Token.ThrowIfCancellationRequested();
+        var ct = GlobalCancel.Token;
+        
+        while (!ct.IsCancellationRequested)
+        {
+            await Task.Delay(10000, ct);
+            var nonHostClients = Clients.Where(c => c.IsClient).ToList();
+            var connectionCountToRotate = (int)Math.Ceiling(nonHostClients.Count * RotationRate);
+
+            if (connectionCountToRotate == 0) continue;
+
+            var victims = new HashSet<MindustryClient>(connectionCountToRotate);
+            do
+            {
+                victims.Add(Random.Shared.RandomItemInList(nonHostClients)!);
+            } while (victims.Count < connectionCountToRotate);
+
+            foreach (var connection in victims)
+            {
+                ct.ThrowIfCancellationRequested();
+
+                connection.Stop();
+                var newConnection = await CreateConnection(HostToTest, HostPortTotest, false);
+                var roomIdToJoin = Random.Shared.RandomItemInList(RoomIds);
+                await newConnection.JoinRoom(roomIdToJoin);
+
+                Console.WriteLine($"Switched connection {connection.ConnectionId} to join room {roomIdToJoin} as {newConnection.ConnectionId}");
+                
+                Clients.Remove(connection);
+                Clients.Add(newConnection);
+
+                await Task.Delay(Random.Shared.Next(25, 100), ct);
+            }
+        }
+    }
+
+    static async Task<MindustryClient> CreateConnection(string host, int port, bool isRoomHost)
+    {
+        var connection = new MindustryClient(host, port, GlobalCancel.Token);
+        await connection.ExecuteHandshake();
+
+        if (isRoomHost)
+        {
+            Console.WriteLine($"{connection.ConnectionId} creating room");
+            await connection.CreateRoom();
+        }
+
+        return connection;
+    }
+
+    private static Action<object?, ConsoleCancelEventArgs> HandleCancellation()
     {
         return (sender, args) =>
         {
             Console.WriteLine("Global cancelling");
-            globalCancel.Cancel();
+            GlobalCancel.Cancel();
         };
     }
 }
---
+++ StressTestHarness/RandomExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace StressTestHarness;
+
+public static class RandomExtensions
+{
+    public static T? RandomItemInList<T>(this Random r, List<T> list)
+    {
+        return list.Count == 0 ? default : list[r.Next(0, list.Count)];
+    }
+}
