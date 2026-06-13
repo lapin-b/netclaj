ce33f5e4079bebf1f1aabd9a94330d8592cba582
l4p1n <l4p1n@l4p1n.ch>
2026-06-13 10:24:44 +0200

    Initial IBufferWriter extensions

---
+++ NetClajServer/Packets/BufferWriterExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Buffers;
+using System.Numerics;
+
+namespace NetClajServer.Packets;
+
+public static class BufferWriterExtensions
+{
+    extension(IBufferWriter<byte> buffer)
+    {
+        public void WriteIntegerBe<T>(T value) where T : IBinaryInteger<T>
+        {
+            var span = buffer.GetSpan(value.GetByteCount());
+            value.WriteBigEndian(span);
+            buffer.Advance(span.Length);
+        }
+    }
+}
---
+++ NetClajServer/Packets/CountingBufferWritter.cs
@@ -0,0 +1,24 @@
+﻿using System.Buffers;
+
+namespace NetClajServer.Packets;
+
+public struct CountingBufferWritter: IBufferWriter<byte>
+{
+    private readonly IBufferWriter<byte> _inner;
+    public int BytesWritten { get; private set; }
+    
+    public CountingBufferWritter(IBufferWriter<byte> inner)
+    {
+        _inner = inner;
+    }
+
+
+    public void Advance(int count)
+    {
+        _inner.Advance(count);
+        BytesWritten += count;
+    }
+
+    public Memory<byte> GetMemory(int sizeHint = 0) => _inner.GetMemory(sizeHint);
+    public Span<byte> GetSpan(int sizeHint = 0) => _inner.GetSpan(sizeHint);
+}
